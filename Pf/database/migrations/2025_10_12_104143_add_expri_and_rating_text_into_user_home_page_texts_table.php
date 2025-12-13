354220d7d3b19bd84994a5110d10e220f0739743
thinkprobd <birgrhapp@gmail.com>
2025-12-12 15:43:43 -0900

    Upload Pf/database/migrations/2025_10_12_104143_add_expri_and_rating_text_into_user_home_page_texts_table.php

---
+++ Pf/database/migrations/2025_10_12_104143_add_expri_and_rating_text_into_user_home_page_texts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user_home_page_texts', function (Blueprint $table) {
+            $table->string('hero_rating_text')->nullable();
+            $table->string('hero_experience_text')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user_home_page_texts', function (Blueprint $table) {
+            $table->dropColumn('hero_rating_text');
+            $table->dropColumn('hero_experience_text');
+        });
+    }
+};
