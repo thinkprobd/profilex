8ceda360f159d7dee4a73439f54fe1a1c8b11477
thinkprobd <birgrhapp@gmail.com>
2025-12-12 15:37:01 -0900

    Upload Pf/app/Http/Middleware/RouteAccess.php

---
+++ Pf/app/Http/Middleware/RouteAccess.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Models\User\UserPermission;
+use App\Http\Helpers\UserPermissionHelper;
+
+class RouteAccess
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next, $page)
+    {
+        $user = getUser();
+        $currentPackage = UserPermissionHelper::userPackage($user->id);
+        $preferences = UserPermission::where([
+            ['user_id', $user->id],
+            ['package_id', $currentPackage->package_id]
+        ])->first();
+        $userPermissions = isset($preferences) ? json_decode($preferences->permissions, true) : [];
+        $packagePermissions = UserPermissionHelper::packagePermission($user->id);
+        $packagePermissions = json_decode($packagePermissions, true);
+
+        if (is_null($userPermissions)) {
+            return redirect()->route('front.user.detail.view', getParam());
+        }
+
+        if ($page == 'Contact' || $page == 'Experience') {
+            if (!in_array($page, $userPermissions)) {
+                return redirect()->route('front.user.detail.view', getParam());
+            }
+        } else {
+            if (!in_array($page, $userPermissions) || !in_array($page, $packagePermissions)) {
+                return redirect()->route('front.user.detail.view', getParam());
+            }
+        }
+        return $next($request);
+    }
+}
