67754cf0fe379bc20bc63eacb95ecd9ba006a417
thinkprobd <birgrhapp@gmail.com>
2025-12-12 15:38:30 -0900

    Upload Pf/app/Models/User/UserVcard.php

---
+++ Pf/app/Models/User/UserVcard.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models\User;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class UserVcard extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function user() {
+        return $this->belongsTo('App\Models\User');
+    }
+
+    public function user_vcard_services() {
+        return $this->hasMany('App\Models\User\UserVcardService');
+    }
+
+    public function user_vcard_projects() {
+        return $this->hasMany('App\Models\User\UserVcardProject');
+    }
+
+    public function user_vcard_testimonials() {
+        return $this->hasMany('App\Models\User\UserVcardTestimonial');
+    }
+
+
+}
