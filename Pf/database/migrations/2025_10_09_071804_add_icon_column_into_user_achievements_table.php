eee2d70a1a0e6ddd492848c7633a3e6820262df2
thinkprobd <birgrhapp@gmail.com>
2025-12-12 15:44:03 -0900

    Upload Pf/database/migrations/2025_10_09_071804_add_icon_column_into_user_achievements_table.php

---
+++ Pf/database/migrations/2025_10_09_071804_add_icon_column_into_user_achievements_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user_achievements', function (Blueprint $table) {
+            $table->string('image')->nullable()->after('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user_achievements', function (Blueprint $table) {
+            $table->dropColumn('image');
+        });
+    }
+};
