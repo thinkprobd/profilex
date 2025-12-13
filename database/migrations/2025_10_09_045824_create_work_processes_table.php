6a52082f84bc4f5f9dd33c9f5f138fb20b8bc77b
thinkprobd <birgrhapp@gmail.com>
2025-12-12 16:37:28 -0900

    Deploy database/migrations/2025_10_09_045824_create_work_processes_table.php

---
+++ database/migrations/2025_10_09_045824_create_work_processes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('work_processes', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('language_id');
+            $table->bigInteger('user_id');
+            $table->string('image')->nullable();
+            $table->string('title')->nullable();
+            $table->string('subtitle')->nullable();
+            $table->integer('serial_number')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('work_processes');
+    }
+};
