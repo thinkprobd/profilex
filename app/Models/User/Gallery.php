932808dc9b5f7b88d25a7092e03f75e23bae537d
thinkprobd <birgrhapp@gmail.com>
2025-12-12 15:09:04 -0900

    Upload app/Models/User/Gallery.php

---
+++ app/Models/User/Gallery.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\User;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Gallery extends Model
+{
+    use HasFactory;
+    protected $guarded = [];
+}
