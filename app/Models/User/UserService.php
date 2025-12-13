e32601f42491e2c0e3a3f5e04701662d300382d9
thinkprobd <birgrhapp@gmail.com>
2025-12-12 15:09:39 -0900

    Upload app/Models/User/UserService.php

---
+++ app/Models/User/UserService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models\User;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserService extends Model
+{
+    public $table = "user_services";
+    protected $fillable= [
+        'icon',
+        'image',
+        'slug',
+        'name',
+        'content',
+        'serial_number',
+        'featured',
+        'detail_page',
+        'user_id',
+        'lang_id',
+        'meta_keywords',
+        'meta_description'
+    ];
+
+    public function language() {
+        return $this->belongsTo(Language::class,'lang_id');
+    }
+}
