458a4ce6658c19ff3122f499d71dec9d880724f8
thinkprobd <birgrhapp@gmail.com>
2025-12-12 15:43:30 -0900

    Upload Pf/database/migrations/2025_10_19_081745_create_galleries_table.php

---
+++ Pf/database/migrations/2025_10_19_081745_create_galleries_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('galleries', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('language_id');
+            $table->bigInteger('user_id');
+            $table->string('name')->nullable();
+            $table->string('image')->nullable();
+            $table->integer('serial_number')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('galleries');
+    }
+};
